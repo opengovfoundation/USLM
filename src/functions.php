9ec4e8d17901e0dbfad024186198a8e6a8f73994
Chris Birk <cmbirk@gmail.com>
2014-10-24 15:33:36 -0400

    only load dd function if it does not exist

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,9 @@
 /**
 * Better array dumping.  Borrowed from the Laravel Framework.
 */
-function dd()
-{
-  array_map(function($x) { var_dump($x); }, func_get_args()); die;
+if(!function_exists('dd')){
+  function dd()
+  {
+    array_map(function($x) { var_dump($x); }, func_get_args()); die;
+  }  
 }
